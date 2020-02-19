e261b7a8ad10b72eb8ff1c6befe3ed58a2a58e18
jiangqiqing <jiangqiqing@miguantech.com>
2020-02-19 11:10:10 +0800

    use plurals to achieve option text selection

--- app/src/main/java/com/example/motionlayoutproject/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutproject/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.motionlayoutproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        main_tv.apply {
+            text = resources.getQuantityString(R.plurals.option_text,1,1)
+            setOnClickListener {
+                text = resources.getQuantityString(R.plurals.option_text,0,7)
+            }
+        }
     }
 }
