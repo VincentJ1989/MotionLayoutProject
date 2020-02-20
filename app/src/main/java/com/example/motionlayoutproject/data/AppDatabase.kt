cefff83707f9696c0da489745c67216f477b6eca
jiangqiqing <jiangqiqing@miguantech.com>
2020-02-20 14:11:11 +0800

    add Converters.kt

--- app/src/main/java/com/example/motionlayoutproject/data/AppDatabase.kt
+++ app/src/main/java/com/example/motionlayoutproject/data/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.motionlayoutproject.DATABASE_NAME
 
@@ -13,6 +14,7 @@ import com.example.motionlayoutproject.DATABASE_NAME
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
